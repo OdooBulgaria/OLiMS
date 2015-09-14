9dff7c8d793e09df5f0588e6c3c9e2aad3c62f68
yasir1brahim <yasir.ibrahim@gmail.com>
2015-09-14 19:28:42 +0500

    Re-factored 'arpriority.py' and added it to Odoo model. Some fields and methods in that file are not yet developed, so these are tagged as 'To be Implemented'.

--- models/arpriority.py
+++ models/arpriority.py
@@ -1,16 +1,27 @@
-from dependencies.dependency import ClassSecurityInfo
-from lims import bikaMessageFactory as _
-from lims.utils import t
-from lims.content.bikaschema import BikaSchema
-from lims.config import PROJECTNAME
-from lims.idserver import renameAfterCreation
-from lims.interfaces import IARPriority
-from dependencies import atapi
-from dependencies.dependency import *
-from dependencies.dependency import implements
+# ~~~~~~~~~~ Irrelevant code for Odoo ~~~~~~~~~~~
+# from dependencies.dependency import ClassSecurityInfo
+# from lims.utils import t
+# from lims.content.bikaschema import BikaSchema
+# from lims.config import PROJECTNAME
+# from lims.interfaces import IARPriority
+# from dependencies import atapi
+# from dependencies.dependency import *
+# from dependencies.dependency import implements
 
-schema = BikaSchema.copy() + Schema((
-    IntegerField('sortKey',
+import logging
+from openerp import models
+
+_logger = logging.getLogger(__name__)
+
+from lims.idserver import renameAfterCreation
+from lims import bikaMessageFactory as _
+from models.base_olims_model import BaseOLiMSModel
+from fields.integer_field import IntegerField
+from fields.boolean_field import BooleanField
+from fields.widget.widget import IntegerWidget, BooleanWidget
+# ~~~~~~~~~~ Irrelevant code for Odoo ~~~~~~~~~~~
+# schema = BikaSchema.copy() + Schema((
+schema = (IntegerField('sortKey',
         widget=IntegerWidget(
             label = _("Sort Key"),
             description = _("Numeric value indicating the sort order of objects that are prioritised"),
@@ -22,38 +33,43 @@ schema = BikaSchema.copy() + Schema((
             description = _("The percentage used to calculate the price for analyses done at this priority"),
         ),
     ),
-    ImageField('smallIcon',
-        widget=ImageWidget(
-            label = _("Small Icon"),
-            description = _("16x16 pixel icon used for the this priority in listings."),
-        ),
-    ),
-    ImageField('bigIcon',
-        widget=ImageWidget(
-            label = _("Big Icon"),
-            description = _("32x32 pixel icon used for the this priority in object views."),
-        ),
-    ),
+# ~~~~~~~ To be implemented ~~~~~~~
+#     ImageField('smallIcon',
+#         widget=ImageWidget(
+#             label = _("Small Icon"),
+#             description = _("16x16 pixel icon used for the this priority in listings."),
+#         ),
+#     ),
+# ~~~~~~~ To be implemented ~~~~~~~
+#     ImageField('bigIcon',
+#         widget=ImageWidget(
+#             label = _("Big Icon"),
+#             description = _("32x32 pixel icon used for the this priority in object views."),
+#         ),
+#     ),
     BooleanField('isDefault',
         widget=BooleanWidget(
             label = _("Default Priority?"),
             description = _("Check this box if this is the default priority"),
         ),
     ),
-))
+)#)
+# ~~~~~~~~~~ Irrelevant code for Odoo ~~~~~~~~~~~
+# schema['description'].widget.visible = True
 
-schema['description'].widget.visible = True
 
-
-class ARPriority(BaseContent):
-    security = ClassSecurityInfo()
-    schema = schema
-    displayContentsTab = False
-    implements(IARPriority)
-    _at_rename_after_creation = True
+class ARPriority(models.Model, BaseOLiMSModel):#(BaseContent):
+    _name = 'olims.ar_priority'
+# ~~~~~~~~~~ Irrelevant code for Odoo ~~~~~~~~~~~
+#     security = ClassSecurityInfo()
+#     schema = schema
+#     displayContentsTab = False
+#     implements(IARPriority)
+#     _at_rename_after_creation = True
 
     def _renameAfterCreation(self, check_auto_id=False):
         renameAfterCreation(self)
 
-
-atapi.registerType(ARPriority, PROJECTNAME)
+ARPriority.initialze(schema)
+# ~~~~~~~~~~ Irrelevant code for Odoo ~~~~~~~~~~~
+# atapi.registerType(ARPriority, PROJECTNAME)
