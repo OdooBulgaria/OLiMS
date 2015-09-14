9e701218bd8453b439e9aacfc9a76c2ed8725f87
yasir1brahim <yasir.ibrahim@gmail.com>
2015-09-14 19:29:19 +0500

    Re-factored 'artemplate.py' and added it to Odoo model. Some fields and methods in that file are not yet developed, so these are tagged as 'To be Implemented'.

--- models/artemplate.py
+++ models/artemplate.py
@@ -2,79 +2,90 @@
     AnalysisRequests often use the same configurations.
     ARTemplate includes all AR fields, including preset AnalysisProfile
 """
-
-from dependencies.dependency import ClassSecurityInfo
-from dependencies.dependency import *
-from dependencies.dependency import HoldingReference
-from dependencies.dependency import RecordsField
+# ~~~~~~~~~~ Irrelevant code for Odoo ~~~~~~~~~~~
+# from dependencies.dependency import ClassSecurityInfo
+# from dependencies.dependency import *
+# from dependencies.dependency import HoldingReference
+# from dependencies.dependency import RecordsField
+# from lims.interfaces import IARTemplate
+# from lims.browser.widgets import RecordsWidget as BikaRecordsWidget
+# from lims.browser.widgets import ARTemplatePartitionsWidget
+# from lims.browser.widgets import ARTemplateAnalysesWidget
+# from lims.browser.widgets import RecordsWidget
+# from lims.browser.widgets import ReferenceWidget
+# from lims.config import PROJECTNAME
+# from lims.content.bikaschema import BikaSchema
+# from dependencies.dependency import Interface, implements
 from dependencies.dependency import getToolByName
-from lims import PMF, bikaMessageFactory as _
-from lims.interfaces import IARTemplate
-from lims.browser.widgets import RecordsWidget as BikaRecordsWidget
-from lims.browser.widgets import ARTemplatePartitionsWidget
-from lims.browser.widgets import ARTemplateAnalysesWidget
-from lims.browser.widgets import RecordsWidget
-from lims.browser.widgets import ReferenceWidget
-from lims.config import PROJECTNAME
-from lims.content.bikaschema import BikaSchema
-from dependencies.dependency import Interface, implements
+from lims import bikaMessageFactory as _
 import sys
+from openerp import models
+from models.base_olims_model import BaseOLiMSModel
+from fields.widget.widget import BooleanWidget, TextAreaWidget
+from fields.boolean_field import BooleanField
+from fields.text_field import TextField
 
-schema = BikaSchema.copy() + Schema((
+# ~~~~~~~~~~ Irrelevant code for Odoo ~~~~~~~~~~~
+# schema = BikaSchema.copy() + Schema(
+schema = (
     ## SamplePoint and SampleType references are managed with
     ## accessors and mutators below to get/set a string value
     ## (the Title of the object), but still store a normal Reference.
     ## Form autocomplete widgets can then work with the Titles.
-    ReferenceField('SamplePoint',
-        vocabulary_display_path_bound = sys.maxint,
-        allowed_types = ('SamplePoint',),
-        relationship = 'ARTemplateSamplePoint',
-        referenceClass = HoldingReference,
-        accessor = 'getSamplePoint',
-        edit_accessor = 'getSamplePoint',
-        mutator = 'setSamplePoint',
-        widget=ReferenceWidget(
-            label = _("Sample Point"),
-            description = _("Location where sample was taken"),
-            visible={'edit': 'visible', 'view': 'visible', 'add': 'visible',
-                     'secondary': 'invisible'},
-            catalog_name='bika_setup_catalog',
-            base_query={'inactive_state': 'active'},
-            showOn=True,
-        ),
-    ),
-    ComputedField(
-        "SamplePointUID",
-        expression="context.Schema()['SamplePoint'].get(context) and context.Schema()['SamplePoint'].get(context).UID() or ''",
-        widget=ComputedWidget(
-            visible=False,
-        ),
-    ),
-    ReferenceField('SampleType',
-        vocabulary_display_path_bound = sys.maxint,
-        allowed_types = ('SampleType',),
-        relationship = 'ARTemplateSampleType',
-        referenceClass = HoldingReference,
-        accessor = 'getSampleType',
-        edit_accessor = 'getSampleType',
-        mutator = 'setSampleType',
-        widget=ReferenceWidget(
-            label = _("Sample Type"),
-            description = _("Create a new sample of this type"),
-            visible={'edit': 'visible', 'view': 'visible', 'add': 'visible',
-                     'secondary': 'invisible'},
-            catalog_name='bika_setup_catalog',
-            base_query={'inactive_state': 'active'},
-            showOn=True,
-        ),
-    ),
-    ComputedField(
-        "SampleTypeUID",
-        expression="context.Schema()['SampleType'].get(context) and context.Schema()['SampleType'].get(context).UID() or ''",
-        widget=ComputedWidget(
-            visible=False,
-        ),
-    ),
+# ~~~~~~~ To be implemented ~~~~~~~
+#     ReferenceField('SamplePoint',
+#         vocabulary_display_path_bound = sys.maxint,
+#         allowed_types = ('SamplePoint',),
+#         relationship = 'ARTemplateSamplePoint',
+#         referenceClass = HoldingReference,
+#         accessor = 'getSamplePoint',
+#         edit_accessor = 'getSamplePoint',
+#         mutator = 'setSamplePoint',
+#         widget=ReferenceWidget(
+#             label = _("Sample Point"),
+#             description = _("Location where sample was taken"),
+#             visible={'edit': 'visible', 'view': 'visible', 'add': 'visible',
+#                      'secondary': 'invisible'},
+#             catalog_name='bika_setup_catalog',
+#             base_query={'inactive_state': 'active'},
+#             showOn=True,
+#         ),
+#     ),
+# ~~~~~~~ To be implemented ~~~~~~~
+#     ComputedField(
+#         "SamplePointUID",
+#         expression="context.Schema()['SamplePoint'].get(context) and context.Schema()['SamplePoint'].get(context).UID() or ''",
+#         widget=ComputedWidget(
+#             visible=False,
+#         ),
+#     ),
+# ~~~~~~~ To be implemented ~~~~~~~
+#     ReferenceField('SampleType',
+#         vocabulary_display_path_bound = sys.maxint,
+#         allowed_types = ('SampleType',),
+#         relationship = 'ARTemplateSampleType',
+#         referenceClass = HoldingReference,
+#         accessor = 'getSampleType',
+#         edit_accessor = 'getSampleType',
+#         mutator = 'setSampleType',
+#         widget=ReferenceWidget(
+#             label = _("Sample Type"),
+#             description = _("Create a new sample of this type"),
+#             visible={'edit': 'visible', 'view': 'visible', 'add': 'visible',
+#                      'secondary': 'invisible'},
+#             catalog_name='bika_setup_catalog',
+#             base_query={'inactive_state': 'active'},
+#             showOn=True,
+#         ),
+#     ),
+# ~~~~~~~ To be implemented ~~~~~~~
+#     ComputedField(
+#         "SampleTypeUID",
+#         expression="context.Schema()['SampleType'].get(context) and context.Schema()['SampleType'].get(context).UID() or ''",
+#         widget=ComputedWidget(
+#             visible=False,
+#         ),
+#     ),
     BooleanField('ReportDryMatter',
         default = False,
         widget = BooleanWidget(
@@ -93,127 +104,135 @@ schema = BikaSchema.copy() + Schema((
             append_only = True,
         ),
     ),
-    RecordsField('Partitions',
-        schemata = 'Sample Partitions',
-        required = 0,
-        type = 'artemplate_parts',
-        subfields = ('part_id',
-                     'Container',
-                     'Preservation',
-                     'container_uid',
-                     'preservation_uid'),
-        subfield_labels = {'part_id': _('Partition'),
-                           'Container': _('Container'),
-                           'Preservation': _('Preservation')},
-        subfield_sizes = {'part_id': 15,
-                           'Container': 35,
-                           'Preservation': 35},
-        subfield_hidden = {'preservation_uid': True,
-                           'container_uid': True},
-        default = [{'part_id':'part-1',
-                    'Container':'',
-                    'Preservation':'',
-                    'container_uid':'',
-                    'preservation_uid':''}],
-        widget=ARTemplatePartitionsWidget(
-            label = _("Sample Partitions"),
-            description = _("Configure the sample partitions and preservations " + \
-                            "for this template. Assign analyses to the different " + \
-                            "partitions on the template's Analyses tab"),
-            combogrid_options={
-                'Container': {
-                    'colModel': [
-                        {'columnName':'container_uid', 'hidden':True},
-                        {'columnName':'Container', 'width':'30', 'label':_('Container')},
-                        {'columnName':'Description', 'width':'70', 'label':_('Description')}],
-                    'url': 'getcontainers',
-                    'showOn': True,
-                    'width': '550px'
-                },
-                'Preservation': {
-                    'colModel': [
-                        {'columnName':'preservation_uid', 'hidden':True},
-                        {'columnName':'Preservation', 'width':'30', 'label':_('Preservation')},
-                        {'columnName':'Description', 'width':'70', 'label':_('Description')}],
-                    'url': 'getpreservations',
-                    'showOn': True,
-                    'width': '550px'
-                },
-            },
-         ),
-    ),
-    ReferenceField('AnalysisProfile',
-        schemata = 'Analyses',
-        required = 0,
-        multiValued = 0,
-        allowed_types = ('AnalysisProfile',),
-        relationship = 'ARTemplateAnalysisProfile',
-        widget=ReferenceWidget(
-            label = _("Analysis Profile"),
-            description =_("The Analysis Profile selection for this template"),
-            visible={'edit': 'visible', 'view': 'visible', 'add': 'visible',
-                     'secondary': 'invisible'},
-            catalog_name='bika_setup_catalog',
-            base_query={'inactive_state': 'active'},
-            showOn=True,
-        ),
-    ),
-    RecordsField('Analyses',
-        schemata = 'Analyses',
-        required = 0,
-        type = 'artemplate_analyses',
-        subfields = ('service_uid', 'partition'),
-        subfield_labels = {'service_uid': _('Title'),
-                           'partition': _('Partition')},
-        default = [],
-        widget = ARTemplateAnalysesWidget(
-            label = _("Analyses"),
-            description=_("Select analyses to include in this template"),
-        )
-    ),
-    # Custom settings for the assigned analysis services
-    # https://jira.bikalabs.com/browse/LIMS-1324
-    # Fields:
-    #   - uid: Analysis Service UID
-    #   - hidden: True/False. Hide/Display in results reports
-    RecordsField('AnalysisServicesSettings',
-         required=0,
-         subfields=('uid', 'hidden',),
-         widget=ComputedWidget(visible=False),
-    ),
-),
-)
+# ~~~~~~~ To be implemented ~~~~~~~
+#     RecordsField('Partitions',
+#         schemata = 'Sample Partitions',
+#         required = 0,
+#         type = 'artemplate_parts',
+#         subfields = ('part_id',
+#                      'Container',
+#                      'Preservation',
+#                      'container_uid',
+#                      'preservation_uid'),
+#         subfield_labels = {'part_id': _('Partition'),
+#                            'Container': _('Container'),
+#                            'Preservation': _('Preservation')},
+#         subfield_sizes = {'part_id': 15,
+#                            'Container': 35,
+#                            'Preservation': 35},
+#         subfield_hidden = {'preservation_uid': True,
+#                            'container_uid': True},
+#         default = [{'part_id':'part-1',
+#                     'Container':'',
+#                     'Preservation':'',
+#                     'container_uid':'',
+#                     'preservation_uid':''}],
+#         widget=ARTemplatePartitionsWidget(
+#             label = _("Sample Partitions"),
+#             description = _("Configure the sample partitions and preservations " + \
+#                             "for this template. Assign analyses to the different " + \
+#                             "partitions on the template's Analyses tab"),
+#             combogrid_options={
+#                 'Container': {
+#                     'colModel': [
+#                         {'columnName':'container_uid', 'hidden':True},
+#                         {'columnName':'Container', 'width':'30', 'label':_('Container')},
+#                         {'columnName':'Description', 'width':'70', 'label':_('Description')}],
+#                     'url': 'getcontainers',
+#                     'showOn': True,
+#                     'width': '550px'
+#                 },
+#                 'Preservation': {
+#                     'colModel': [
+#                         {'columnName':'preservation_uid', 'hidden':True},
+#                         {'columnName':'Preservation', 'width':'30', 'label':_('Preservation')},
+#                         {'columnName':'Description', 'width':'70', 'label':_('Description')}],
+#                     'url': 'getpreservations',
+#                     'showOn': True,
+#                     'width': '550px'
+#                 },
+#             },
+#          ),
+#     ),
+# ~~~~~~~ To be implemented ~~~~~~~
+#     ReferenceField('AnalysisProfile',
+#         schemata = 'Analyses',
+#         required = 0,
+#         multiValued = 0,
+#         allowed_types = ('AnalysisProfile',),
+#         relationship = 'ARTemplateAnalysisProfile',
+#         widget=ReferenceWidget(
+#             label = _("Analysis Profile"),
+#             description =_("The Analysis Profile selection for this template"),
+#             visible={'edit': 'visible', 'view': 'visible', 'add': 'visible',
+#                      'secondary': 'invisible'},
+#             catalog_name='bika_setup_catalog',
+#             base_query={'inactive_state': 'active'},
+#             showOn=True,
+#         ),
+#     ),
+# ~~~~~~~ To be implemented ~~~~~~~
+#     RecordsField('Analyses',
+#         schemata = 'Analyses',
+#         required = 0,
+#         type = 'artemplate_analyses',
+#         subfields = ('service_uid', 'partition'),
+#         subfield_labels = {'service_uid': _('Title'),
+#                            'partition': _('Partition')},
+#         default = [],
+#         widget = ARTemplateAnalysesWidget(
+#             label = _("Analyses"),
+#             description=_("Select analyses to include in this template"),
+#         )
+#     ),
+#     # Custom settings for the assigned analysis services
+#     # https://jira.bikalabs.com/browse/LIMS-1324
+#     # Fields:
+#     #   - uid: Analysis Service UID
+#     #   - hidden: True/False. Hide/Display in results reports
+# ~~~~~~~ To be implemented ~~~~~~~
+#     RecordsField('AnalysisServicesSettings',
+#          required=0,
+#          subfields=('uid', 'hidden',),
+#          widget=ComputedWidget(visible=False),
+#     ),
+)#,
+#)
 
-schema['description'].widget.visible = True
-schema['title'].widget.visible = True
-schema['title'].validators = ('uniquefieldvalidator',)
-# Update the validation layer after change the validator in runtime
-schema['title']._validationLayer()
+# ~~~~~~~~~~ Irrelevant code for Odoo ~~~~~~~~~~~
+# schema['description'].widget.visible = True
+# schema['title'].widget.visible = True
+# schema['title'].validators = ('uniquefieldvalidator',)
+# # Update the validation layer after change the validator in runtime
+# schema['title']._validationLayer()
 
-class ARTemplate(BaseContent):
-    security = ClassSecurityInfo()
-    schema = schema
-    displayContentsTab = False
-    implements(IARTemplate)
+class ARTemplate(models.Model, BaseOLiMSModel):#(BaseContent):
+    _name = 'olims.ar_template'
+# ~~~~~~~~~~ Irrelevant code for Odoo ~~~~~~~~~~~
+#     security = ClassSecurityInfo()
+#     schema = schema
+#     displayContentsTab = False
+#     implements(IARTemplate)
 
     _at_rename_after_creation = True
     def _renameAfterCreation(self, check_auto_id=False):
         from lims.idserver import renameAfterCreation
         renameAfterCreation(self)
-
-    security.declarePublic('AnalysisProfiles')
-    def AnalysisProfiles(self, instance=None):
-        instance = instance or self
-        bsc = getToolByName(instance, 'bika_setup_catalog')
-        items = []
-        for p in bsc(portal_type='AnalysisProfile',
-                      inactive_state='active',
-                      sort_on = 'sortable_title'):
-            p = p.getObject()
-            title = p.Title()
-            items.append((p.UID(), title))
-        items = [['','']] + list(items)
-        return DisplayList(items)
+# ~~~~~~~~~~ Irrelevant code for Odoo ~~~~~~~~~~~
+#     security.declarePublic('AnalysisProfiles')
+# ~~~~~~~ To be implemented ~~~~~~~
+#     def AnalysisProfiles(self, instance=None):
+#         instance = instance or self
+#         bsc = getToolByName(instance, 'bika_setup_catalog')
+#         items = []
+#         for p in bsc(portal_type='AnalysisProfile',
+#                       inactive_state='active',
+#                       sort_on = 'sortable_title'):
+#             p = p.getObject()
+#             title = p.Title()
+#             items.append((p.UID(), title))
+#         items = [['','']] + list(items)
+#         return DisplayList(items)
 
     def getClientUID(self):
         return self.aq_parent.UID()
@@ -250,4 +269,6 @@ class ARTemplate(BaseContent):
                 raise ValueError('%s is not valid' % uid)
         return sets.get('hidden', False)
 
-registerType(ARTemplate, PROJECTNAME)
+ARTemplate.initialze(schema)    
+# ~~~~~~~~~~ Irrelevant code for Odoo ~~~~~~~~~~~
+# registerType(ARTemplate, PROJECTNAME)
