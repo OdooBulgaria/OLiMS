ca71af53bed52264943e81d02d44de7f5c3afa71
yasir1brahim <yasir.ibrahim@gmail.com>
2015-09-14 19:25:34 +0500

    Re-factored 'analysisprofile.py' and added it to Odoo model. Some fields and methods in 'analysisprofile.py' are not yet developed, so these are tagged as 'To be Implemented'.

--- models/analysisprofile.py
+++ models/analysisprofile.py
@@ -2,41 +2,60 @@
     AnalysisRequests often use the same configurations.
     AnalysisProfile is used to save these common configurations (templates).
 """
-import os
-import sys
 import logging
+
+from openerp import fields, models,osv
 
 _logger = logging.getLogger(__name__)
 
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
 
 from dependencies.dependency import ClassSecurityInfo
 from lims import PMF, bikaMessageFactory as _
 
-# ~~~~~~~~~~  Irrelevant code for Odoo ~~~~~~~~~~~
+# ~~~~~~~~~~ Irrelevant code for Odoo ~~~~~~~~~~~
 # from lims.browser.widgets import AnalysisProfileAnalysesWidget
 # from lims.browser.widgets import ServicesWidget
 # from lims.content.bikaschema import BikaSchema
 # from lims.interfaces import IAnalysisProfile
 # from dependencies.dependency import Interface, implements
+# from dependencies.dependency import *
+# from dependencies.dependency import RecordsField
+# from lims.config import PROJECTNAME
+# from dependencies.dependency import HoldingReference
 
-from lims.config import PROJECTNAME
-from dependencies.dependency import *
-from dependencies.dependency import HoldingReference
-from dependencies.dependency import RecordsField
+
 from dependencies.dependency import getToolByName
-import sys
+from models.base_olims_model import BaseOLiMSModel
+from fields.string_field import StringField
+from fields.text_field import TextField
+from fields.boolean_field import BooleanField
+from fields.fixed_point_field import FixedPointField
+from fields.widget.widget import StringWidget, AnalysisProfileAnalysesWidget, \
+                                 TextAreaWidget, ComputedWidget, \
+                                 BooleanWidget, DecimalWidget
 
 # ~~~~~ Useful code that need to be converted in Odoo style ~~~~~~ 
-# schema = BikaSchema.copy() + Schema((
-#     StringField('ProfileKey',
-#         widget = StringWidget(
-#             label = _("Profile Keyword"),
-#             description = _("The profile's keyword is used to uniquely identify " + \
-#                           "it in import files. It has to be unique, and it may " + \
-#                           "not be the same as any Calculation Interim field ID."),
-#         ),
-#     ),
+# schema = BikaSchema.copy() + Schema(
+
+schema = (StringField('Title',
+              required=1,        
+    ),
+    TextField('Description',
+        widget = TextAreaWidget(
+            label=_('Description'),
+            description=_('Used in item listings and search results.'),
+        ),
+    ),                 
+    StringField('ProfileKey',
+        widget = StringWidget(
+            label = _("Profile Keyword"),
+            description = _("The profile's keyword is used to uniquely identify " + \
+                          "it in import files. It has to be unique, and it may " + \
+                          "not be the same as any Calculation Interim field ID."),
+        ),
+    ),
+# ~~~~~~~ To be implemented ~~~~~~~
 #     ReferenceField('Service',
 #         schemata = 'Analyses',
 #         required = 1,
@@ -48,73 +67,79 @@ import sys
 #             description = _("The analyses included in this profile, grouped per category"),
 #         )
 #     ),
-#     TextField('Remarks',
-#         searchable = True,
-#         default_content_type = 'text/plain',
-#         allowable_content_types = ('text/plain', ),
-#         default_output_type="text/plain",
-#         widget = TextAreaWidget(
-#             macro = "bika_widgets/remarks",
-#             label = _("Remarks"),
-#             append_only = True,
-#         ),
-#     ),
-#     # Custom settings for the assigned analysis services
-#     # https://jira.bikalabs.com/browse/LIMS-1324
-#     # Fields:
-#     #   - uid: Analysis Service UID
-#     #   - hidden: True/False. Hide/Display in results reports
+    TextField('Remarks',
+        searchable = True,
+        default_content_type = 'text/plain',
+        allowable_content_types = ('text/plain', ),
+        default_output_type="text/plain",
+        widget = TextAreaWidget(
+            macro = "bika_widgets/remarks",
+            label = _("Remarks"),
+            append_only = True,
+        ),
+    ),
+    # Custom settings for the assigned analysis services
+    # https://jira.bikalabs.com/browse/LIMS-1324
+    # Fields:
+    #   - uid: Analysis Service UID
+    #   - hidden: True/False. Hide/Display in results reports
+    
+# ~~~~~~~ To be implemented ~~~~~~~
+    
 #     RecordsField('AnalysisServicesSettings',
 #          required=0,
 #          subfields=('uid', 'hidden',),
 #          widget=ComputedWidget(visible=False),
 #     ),
-#     StringField('CommercialID',
-#         searchable=1,
-#         required=0,
-#         schemata='Accounting',
-#         widget=StringWidget(
-#             visible={'view': 'visible', 'edit': 'visible'},
-#             label=_('Commercial ID'),
-#             description=_("The profile's commercial ID for accounting purposes."),
-#         ),
-#     ),
-#     # When it's set, the system uses the analysis profile's price to quote and the system's VAT is overridden by the
-#     # the analysis profile's specific VAT
-#     BooleanField('UseAnalysisProfilePrice',
-#         default=False,
-#         schemata='Accounting',
-#         widget=BooleanWidget(
-#             label=_("Use Analysis Profile Price"),
-#             description=_("When it's set, the system uses the analysis profile's price to quote and the system's VAT is"
-#                           " overridden by the analysis profile's specific VAT"),
-#         )
-#     ),
-#     # The price will only be used if the checkbox "use analysis profiles' price" is set.
-#     # This price will be used to quote the analyses instead of analysis service's price.
-#     FixedPointField('AnalysisProfilePrice',
-#         schemata="Accounting",
-#         default='0.00',
-#         widget=DecimalWidget(
-#             label = _("Price (excluding VAT)"),
-#             visible={'view': 'visible', 'edit': 'visible'},
-#         ),
-#     ),
-#     # When the checkbox "use analysis profiles' price" is set, the AnalysisProfilesVAT should override
-#     # the system's VAT
-#     FixedPointField('AnalysisProfileVAT',
-#         schemata = "Accounting",
-#         default = '14.00',
-#         widget = DecimalWidget(
-#             label=_("VAT %"),
-#             description=_(
-#                 "Enter percentage value eg. 14.0. This percentage is applied on the Analysis Profile only, overriding "
-#                 "the systems VAT"),
-#                 visible={'view': 'visible', 'edit': 'visible'},
-#         )
-#     ),
-#     # This VAT amount is computed using the AnalysisProfileVAT instead of systems VAT
+
+    StringField('CommercialID',
+        searchable=1,
+        required=0,
+        schemata='Accounting',
+        widget=StringWidget(
+            visible={'view': 'visible', 'edit': 'visible'},
+            label=_('Commercial ID'),
+            description=_("The profile's commercial ID for accounting purposes."),
+        ),
+    ),
+    # When it's set, the system uses the analysis profile's price to quote and the system's VAT is overridden by the
+    # the analysis profile's specific VAT
+    BooleanField('UseAnalysisProfilePrice',
+        default=False,
+        schemata='Accounting',
+        widget=BooleanWidget(
+            label=_("Use Analysis Profile Price"),
+            description=_("When it's set, the system uses the analysis profile's price to quote and the system's VAT is"
+                          " overridden by the analysis profile's specific VAT"),
+        )
+    ),
+    # The price will only be used if the checkbox "use analysis profiles' price" is set.
+    # This price will be used to quote the analyses instead of analysis service's price.
+    FixedPointField('AnalysisProfilePrice',
+        schemata="Accounting",
+        default='0.00',
+        widget=DecimalWidget(
+            label = _("Price (excluding VAT)"),
+            visible={'view': 'visible', 'edit': 'visible'},
+        ),
+    ),
+    # When the checkbox "use analysis profiles' price" is set, the AnalysisProfilesVAT should override
+    # the system's VAT
+    FixedPointField('AnalysisProfileVAT',
+        schemata = "Accounting",
+        default = '14.00',
+        widget = DecimalWidget(
+            label=_("VAT %"),
+            description=_(
+                "Enter percentage value eg. 14.0. This percentage is applied on the Analysis Profile only, overriding "
+                "the systems VAT"),
+                visible={'view': 'visible', 'edit': 'visible'},
+        )
+    ),
+    # This VAT amount is computed using the AnalysisProfileVAT instead of systems VAT
+# ~~~~~~~ To be implemented ~~~~~~~
 #     ComputedField('VATAmount',
+#         coumpute='getVATAmount',       
 #         schemata="Accounting",
 #         expression='context.getVATAmount()',
 #         widget=ComputedWidget(
@@ -124,27 +149,25 @@ import sys
 #     ),
 #     ComputedField('TotalPrice',
 #           schemata="Accounting",
+#           coumpute='getTotalPrice',
 #           expression='context.getTotalPrice()',
 #           widget=ComputedWidget(
 #               label = _("Total price"),
 #               visible={'edit': 'hidden', }
 #           ),
 #     ),
-# )
-# )
+)
+#)
 
-
-
-
-
-
-"""      Irrelevant code for Odoo     """
+# ~~~~~~~~~~  Irrelevant code for Odoo ~~~~~~~~~~~
 # schema['title'].widget.visible = True
 # schema['description'].widget.visible = True
 # IdField = schema['id']
 
-class AnalysisProfile(BaseContent):
-#"""      Irrelevant code for Odoo     """
+class AnalysisProfile(models.Model, BaseOLiMSModel):
+    _name = "olims.analysis_profile"
+    
+# ~~~~~~~~~~  Irrelevant code for Odoo ~~~~~~~~~~~
 #     security = ClassSecurityInfo()
 #     schema = schema
 #     displayContentsTab = False
@@ -202,5 +225,8 @@ class AnalysisProfile(BaseContent):
         """
         price, vat = self.getAnalysisProfilePrice(), self.getVATAmount()
         return float(price) + float(vat)
+    
+# ~~~~~~~~~~  Irrelevant code for Odoo ~~~~~~~~~~~
+# registerType(AnalysisProfile, PROJECTNAME)
 
-registerType(AnalysisProfile, PROJECTNAME)
+AnalysisProfile.initialze(schema)
