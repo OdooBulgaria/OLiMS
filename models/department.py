52690594765c089c5620799d88e50ef298b02fc4
yasir1brahim <yasir.ibrahim@gmail.com>
2015-09-15 12:54:42 +0500

    Re-factored 'department.py' and added it to Odoo model. Some fields and methods in that file are not yet developed, so these are tagged as 'To be Implemented'.

--- models/department.py
+++ models/department.py
@@ -1,62 +1,101 @@
 """Department - the department in the laboratory.
 """
-from dependencies.dependency import *
-from dependencies.dependency import HoldingReference
-from dependencies.dependency import getToolByName
-from lims.config import PROJECTNAME
-from lims.content.bikaschema import BikaSchema
-from dependencies.dependency import ClassSecurityInfo
+# ~~~~~~~~~~ Irrelevant code for Odoo ~~~~~~~~~~~
+# from dependencies.dependency import *
+# from dependencies.dependency import HoldingReference
+# from lims.content.bikaschema import BikaSchema
+# from dependencies.dependency import ClassSecurityInfo
+# from lims.utils import t
+# from dependencies.dependency import implements
+
+import logging
+
+from openerp import fields, models,osv
+
+_logger = logging.getLogger(__name__)
+
 import sys
 from lims import bikaMessageFactory as _
-from lims.utils import t
-from dependencies.dependency import implements
+from dependencies.dependency import getToolByName
+from lims.config import PROJECTNAME
+# from dependencies.fields import StringField, TextField, ReferenceField
+from fields.string_field import StringField
+from fields.text_field import TextField
+from fields.widget.widget import StringWidget, TextAreaWidget, ReferenceWidget
+from models.base_olims_model import BaseOLiMSModel
 
-schema = BikaSchema.copy() + Schema((
-    ReferenceField('Manager',
-        vocabulary = 'getContacts',
-        vocabulary_display_path_bound = sys.maxint,
-        allowed_types = ('LabContact',),
-        referenceClass = HoldingReference,
-        relationship = 'DepartmentLabContact',
-        widget = ReferenceWidget(
-            checkbox_bound = 0,
-            label=_("Manager"),
-            description = _(
-                "Select a manager from the available personnel configured under the "
-                "'lab contacts' setup item. Departmental managers are referenced on "
-                "analysis results reports containing analyses by their department."),
+# schema = BikaSchema.copy() + Schema(
+schema = (
+    StringField('Title',
+        required=1,
+        widget=StringWidget(
+            label=_('Title'),
+            description=_('Title is required.'),
         ),
     ),
-    ComputedField('ManagerName',
-        expression = "context.getManager() and context.getManager().getFullname() or ''",
-        widget = ComputedWidget(
-            visible = False,
+    TextField('Description',
+        widget=TextAreaWidget(
+            label=_('Description'),
+            description=_('Used in item listings and search results.'),
         ),
     ),
-    ComputedField('ManagerPhone',
-        expression = "context.getManager() and context.getManager().getBusinessPhone() or ''",
-        widget = ComputedWidget(
-            visible = False,
-        ),
-    ),
-    ComputedField('ManagerEmail',
-        expression = "context.getManager() and context.getManager().getEmailAddress() or ''",
-        widget = ComputedWidget(
-            visible = False,
-        ),
-    ),
-))
-schema['description'].widget.visible = True
-schema['description'].schemata = 'default'
+# ~~~~~~~ To be implemented ~~~~~~~
+#     ReferenceField(string='Manager',
+#         selection=[('olims.lab_contact', 'Lab Manager')],
+#         vocabulary = 'getContacts',
+#         vocabulary_display_path_bound = sys.maxint,
+#         allowed_types = ('LabContact',),
+# #         referenceClass = HoldingReference,
+#         relationship = 'DepartmentLabContact',
+#         widget = ReferenceWidget(
+#             checkbox_bound = 0,
+#             label=_("Manager"),
+#             description = _(
+#                 "Select a manager from the available personnel configured under the "
+#                 "'lab contacts' setup item. Departmental managers are referenced on "
+#                 "analysis results reports containing analyses by their department."),
+#         ),
+#     ),
+# ~~~~~~~ To be implemented ~~~~~~~
+#     ComputedField('ManagerName',
+#         expression = "context.getManager() and context.getManager().getFullname() or ''",
+#         widget = ComputedWidget(
+#             visible = False,
+#         ),
+#     ),
+# ~~~~~~~ To be implemented ~~~~~~~
+#     ComputedField('ManagerPhone',
+#         expression = "context.getManager() and context.getManager().getBusinessPhone() or ''",
+#         widget = ComputedWidget(
+#             visible = False,
+#         ),
+#     ),
+# ~~~~~~~ To be implemented ~~~~~~~
+#     ComputedField('ManagerEmail',
+#         expression = "context.getManager() and context.getManager().getEmailAddress() or ''",
+#         widget = ComputedWidget(
+#             visible = False,
+#         ),
+#     ),
+)#)
 
-class Department(BaseContent):
-    security = ClassSecurityInfo()
-    displayContentsTab = False
-    schema = schema
+# ~~~~~~~~~~ Irrelevant code for Odoo ~~~~~~~~~~~
+# schema['description'].widget.visible = True
+# schema['description'].schemata = 'default'
 
+
+class Department(models.Model, BaseOLiMSModel):
+    _name = 'olims.department'
+# ~~~~~~~~~~ Irrelevant code for Odoo ~~~~~~~~~~~
+#     security = ClassSecurityInfo()
+#     displayContentsTab = False
+#     schema = schema
+        
     _at_rename_after_creation = True
     def _renameAfterCreation(self, check_auto_id=False):
         from lims.idserver import renameAfterCreation
         renameAfterCreation(self)
 
-registerType(Department, PROJECTNAME)
+Department.initialze(schema)
+# ~~~~~~~~~~ Irrelevant code for Odoo ~~~~~~~~~~~
+# registerType(Department, PROJECTNAME)
