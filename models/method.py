b1b1f6462165629e8ea0e9a3c21f69d4b416e47d
yasir1brahim <yasir.ibrahim@gmail.com>
2015-09-15 13:05:38 +0500

    Re-factored 'method.py' and added it to Odoo model. Some fields and methods in that file are not yet developed, so these are tagged as 'To be Implemented'.

--- models/method.py
+++ models/method.py
@@ -1,24 +1,57 @@
-from dependencies.dependency import ClassSecurityInfo
+# ~~~~~~~~~~ Irrelevant code for Odoo ~~~~~~~~~~~
+# from dependencies.dependency import ClassSecurityInfo
+# from dependencies.dependency import *
+# from dependencies.dependency import HoldingReference
+# from dependencies.dependency import RecordsField as RecordsField
+# from lims.browser.fields import HistoryAwareReferenceField
+# from lims.browser.widgets import RecordsWidget
+# from lims.content.bikaschema import BikaSchema
+# from lims.interfaces import IMethod
+# from lims.utils import to_utf8
+# from dependencies.dependency import implements
+
+import logging
+
+from openerp import fields, models,osv
+
+_logger = logging.getLogger(__name__)
+
 from dependencies.dependency import getToolByName
-from dependencies.dependency import *
-from dependencies.dependency import HoldingReference
-from dependencies.dependency import RecordsField as RecordsField
-from lims.browser.fields import HistoryAwareReferenceField
-from lims.browser.widgets import RecordsWidget
-from lims.content.bikaschema import BikaSchema
 from lims.config import PROJECTNAME
 import sys
 from lims import bikaMessageFactory as _
 from lims.utils import t
-from lims.interfaces import IMethod
-from lims.utils import to_utf8
-from dependencies.dependency import implements
+from dependencies.dependency import DisplayList
+from models.base_olims_model import BaseOLiMSModel
+from fields.string_field import StringField
+from fields.text_field import TextField
+from fields.boolean_field import BooleanField
+from fields.file_field import FileField
+from fields.widget.widget import StringWidget, TextAreaWidget, \
+                                FileWidget, BooleanWidget, \
+                                ReferenceWidget, MultiSelectionWidget
 
-schema = BikaSchema.copy() + Schema((
+# ~~~~~~~~~~ Irrelevant code for Odoo ~~~~~~~~~~~
+# schema = BikaSchema.copy() + Schema(
+schema = (
+    StringField('Title',
+        required=True,
+        widget=StringWidget(
+            label=_('Title'),
+            description=_('Title is required.'),
+        ),
+    ),
+    TextField('Description',
+        widget=TextAreaWidget(
+            label=_('Description'),
+            description=_('Used in item listings and search results.'),
+        ),
+    ),
     # Method ID should be unique, specified on MethodSchemaModifier
     StringField('MethodID',
         searchable=1,
         required=0,
+        help='Define an identifier code for the method. It must be unique.',
         validators=('uniquefieldvalidator',),
         widget=StringWidget(
             visible={'view': 'visible', 'edit': 'visible'},
@@ -45,27 +78,42 @@ schema = BikaSchema.copy() + Schema((
 
     # The instruments linked to this method. Don't use this
     # method, use getInstrumentUIDs() or getInstruments() instead
-    LinesField('_Instruments',
-        vocabulary='getInstrumentsDisplayList',
-        widget=MultiSelectionWidget(
-            modes = ('edit'),
-            label=_("Instruments"),
-            description =_(
-                "The selected instruments have support for this method. "
-                "Use the Instrument edit view to assign "
-                "the method to a specific instrument"),
-        ),
-    ),
+# ~~~~~~~ To be implemented ~~~~~~~
+#     LinesField('_Instruments',
+#         vocabulary='getInstrumentsDisplayList',
+#         widget=MultiSelectionWidget(
+#             modes = ('edit'),
+#             label=_("Instruments"),
+#             description =_(
+#                 "The selected instruments have support for this method. "
+#                 "Use the Instrument edit view to assign "
+#                 "the method to a specific instrument"),
+#         ),
+#     ),
+# ~~~~~~~ To be implemented ~~~~~~~
+#     LinesField(string='Instruments',
+#         comodel_name='olims.instrument_type',
+#         vocabulary='getInstrumentsDisplayList',
+#         widget=MultiSelectionWidget(
+#             modes = ('edit'),
+#             label=_("Instruments"),
+#             description =_(
+#                 "The selected instruments have support for this method. "
+#                 "Use the Instrument edit view to assign "
+#                 "the method to a specific instrument"),
+#         ),
+#     ),
 
     # All the instruments available in the system. Don't use this
     # method to retrieve the instruments linked to this method, use
     # getInstruments() or getInstrumentUIDs() instead.
-    LinesField('_AvailableInstruments',
-        vocabulary='_getAvailableInstrumentsDisplayList',
-        widget=MultiSelectionWidget(
-            modes = ('edit'),
-        )
-    ),
+# ~~~~~~~ To be implemented ~~~~~~~
+#     LinesField('_AvailableInstruments',
+#         vocabulary='_getAvailableInstrumentsDisplayList',
+#         widget=MultiSelectionWidget(
+#             modes = ('edit'),
+#         )
+#     ),
 
     # If no instrument selected, always True. Otherwise, the user will
     # be able to set or unset the value. The behavior for this field
@@ -80,36 +128,41 @@ schema = BikaSchema.copy() + Schema((
     ),
 
     # Only shown in readonly view. Not in edit view
-    ComputedField('ManualEntryOfResultsViewField',
-        expression = "context.isManualEntryOfResults()",
-        widget = BooleanWidget(
-            label=_("Manual entry of results"),
-            description=_("The results for the Analysis Services that use this method can be set manually"),
-            modes = ('view'),
-        ),
-    ),
+# ~~~~~~~ To be implemented ~~~~~~~
+#     ComputedField('ManualEntryOfResultsViewField',
+#         expression = "context.isManualEntryOfResults()",
+#         widget = BooleanWidget(
+#             label=_("Manual entry of results"),
+#             description=_("The results for the Analysis Services that use this method can be set manually"),
+#             modes = ('view'),
+#         ),
+#     ),
 
     # Calculations associated to this method. The analyses services
     # with this method assigned will use the calculation selected here.
-    HistoryAwareReferenceField('Calculation',
-        required = 0,
-        vocabulary_display_path_bound = sys.maxint,
-        vocabulary = '_getCalculations',
-        allowed_types = ('Calculation',),
-        relationship = 'MethodCalculation',
-        referenceClass = HoldingReference,
-        widget = ReferenceWidget(
-            checkbox_bound = 0,
-            label=_("Calculation"),
-            description =_("If required, select a calculation for the "
-                           "The analysis services linked to this "
-                           "method. Calculations can be configured "
-                           "under the calculations item in the LIMS "
-                           "set-up"),
-            catalog_name='bika_setup_catalog',
-            base_query={'inactive_state': 'active'},
-        )
-    ),
+# ~~~~~~~ To be implemented ~~~~~~~
+#     HistoryAwareReferenceField(string='Calculation',
+#         comodel_name='olims.calculation',
+#         column1='Title',
+#         required = 0,
+#         vocabulary_display_path_bound = sys.maxint,
+#         vocabulary = '_getCalculations',
+#         allowed_types = ('Calculation',),
+#         relationship = 'MethodCalculation',
+# ~~~~~~~ To be implemented ~~~~~~~        
+# #         referenceClass = HoldingReference,
+#         widget = ReferenceWidget(
+#             checkbox_bound = 0,
+#             label=_("Calculation"),
+#             description =_("If required, select a calculation for the "
+#                            "The analysis services linked to this "
+#                            "method. Calculations can be configured "
+#                            "under the calculations item in the LIMS "
+#                            "set-up"),
+#             catalog_name='bika_setup_catalog',
+#             base_query={'inactive_state': 'active'},
+#         )
+#     ),
     BooleanField('Accredited',
         schemata="default",
         default=True,
@@ -117,19 +170,22 @@ schema = BikaSchema.copy() + Schema((
             label=_("Accredited"),
             description=_("Check if the method has been accredited"))
     ),
-))
+)#)
 
-schema['description'].schemata = 'default'
-schema['description'].widget.visible = True
-schema['description'].widget.label = _("Description")
-schema['description'].widget.description = _("Describes the method in layman terms. This information is made available to lab clients")
+# ~~~~~~~~~~ Irrelevant code for Odoo ~~~~~~~~~~~
+# schema['description'].schemata = 'default'
+# schema['description'].widget.visible = True
+# schema['description'].widget.label = _("Description")
+# schema['description'].widget.description = _("Describes the method in layman terms. This information is made available to lab clients")
 
-class Method(BaseFolder):
-    implements(IMethod)
-    security = ClassSecurityInfo()
-    displayContentsTab = False
-    schema = schema
-
+class Method(models.Model, BaseOLiMSModel):#(BaseFolder):
+    _name = 'olims.method'
+# ~~~~~~~~~~ Irrelevant code for Odoo ~~~~~~~~~~~      
+#     implements(IMethod)
+#     security = ClassSecurityInfo()
+#     displayContentsTab = False
+#     schema = schema
+    
     _at_rename_after_creation = True
     def _renameAfterCreation(self, check_auto_id=False):
         from lims.idserver import renameAfterCreation
@@ -183,6 +239,8 @@ class Method(BaseFolder):
                              inactive_state = 'active')]
         items.sort(lambda x,y: cmp(x[1], y[1]))
         return DisplayList(list(items))
+    
+Method.initialze(schema)    
 
-
-registerType(Method, PROJECTNAME)
+# ~~~~~~~~~~ Irrelevant code for Odoo ~~~~~~~~~~~
+# registerType(Method, PROJECTNAME)
