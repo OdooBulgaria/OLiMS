da950f2dbbd4e492feb311a8e2e7213439527fc6
yasir1brahim <yasir.ibrahim@gmail.com>
2015-09-15 12:58:16 +0500

    Copied the 'instrumenttype.py' file  in Odoo model from the content folder of Bika  to use/convert Bika content class as Odoo model

---
+++ models/instrumenttype.py
@@ -0,0 +1,26 @@
+from dependencies.dependency import ClassSecurityInfo
+from dependencies.dependency import *
+from lims import bikaMessageFactory as _
+from lims.utils import t
+from lims.config import PROJECTNAME
+from lims.content.bikaschema import BikaSchema
+from lims.interfaces import IInstrumentType
+from dependencies.dependency import implements
+
+schema = BikaSchema.copy()
+
+schema['description'].schemata = 'default'
+schema['description'].widget.visible = True
+
+class InstrumentType(BaseContent):
+    implements(IInstrumentType)
+    security = ClassSecurityInfo()
+    displayContentsTab = False
+    schema = schema
+
+    _at_rename_after_creation = True
+    def _renameAfterCreation(self, check_auto_id=False):
+        from lims.idserver import renameAfterCreation
+        renameAfterCreation(self)
+
+registerType(InstrumentType, PROJECTNAME)
