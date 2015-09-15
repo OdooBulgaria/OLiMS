a509c199b0d0e9d1ca50246ff9c95b891836535d
yasir1brahim <yasir.ibrahim@gmail.com>
2015-09-15 12:33:08 +0500

    Re-factored 'analysiscategory.py' and added it to Odoo model. Some fields and methods in that file are not yet developed, so these are tagged as 'To be Implemented'.

--- models/analysiscategory.py
+++ models/analysiscategory.py
@@ -1,20 +1,40 @@
 """Analysis Category - the category of the analysis service
 """
-from dependencies.dependency import ClassSecurityInfo
+# ~~~~~~~~~~ Irrelevant code for Odoo ~~~~~~~~~~~
+# from dependencies.dependency import ClassSecurityInfo
+# from lims.utils import t
+# from lims.config import PROJECTNAME
+# from lims.content.bikaschema import BikaSchema
+# from lims.interfaces import IAnalysisCategory
+# from dependencies.dependency import implements
+# from dependencies.dependency import HoldingReference
+# from dependencies.dependency import DisplayList
+
+import logging
+
+from openerp import fields, models
+
+_logger = logging.getLogger(__name__)
 from lims import bikaMessageFactory as _
-from lims.utils import t
-from lims.config import PROJECTNAME
-from lims.content.bikaschema import BikaSchema
-from lims.interfaces import IAnalysisCategory
-from dependencies.dependency import *
-from dependencies.dependency import HoldingReference
 from dependencies.dependency import getToolByName
 from dependencies.dependency import WorkflowException
-from dependencies.dependency import implements
+from fields.string_field import StringField
+from fields.text_field import TextField
+from fields.widget.widget import TextAreaWidget
+from models.base_olims_model import BaseOLiMSModel
 import sys
 from dependencies import transaction
 
-schema = BikaSchema.copy() + Schema((
+# ~~~~~~~~~~ Irrelevant code for Odoo ~~~~~~~~~~~
+# schema = BikaSchema.copy() + Schema(
+schema =  (StringField('Title',
+              required=1,        
+    ),
+    TextField('Description',
+        widget=TextAreaWidget(
+            description = _('Used in item listings and search results.'),
+                            )
+    ),
     TextField('Comments',
         default_output_type = 'text/plain',
         allowable_content_types = ('text/plain',),
@@ -23,49 +43,47 @@ schema = BikaSchema.copy() + Schema((
                             "Category section on results reports."),
             label = _("Comments")),
     ),
-    ReferenceField('Department',
-        required=1,
-        vocabulary='getDepartments',
-        vocabulary_display_path_bound=sys.maxsize,
-        allowed_types=('Department',),
-        relationship='AnalysisCategoryDepartment',
-        referenceClass=HoldingReference,
-        widget=ReferenceWidget(
-            checkbox_bound=0,
-            label = _("Department"),
-            description = _("The laboratory department"),
-        ),
+# ~~~~~~~ Using Odoo fields.Reference ~~~~~~~
+    fields.Reference(string='Department',
+        selection=[('olims.department', 'Department')],
+        required=True,
     ),
-    ComputedField('DepartmentTitle',
-        expression="context.getDepartment() and context.getDepartment().Title() or ''",
-        widget=ComputedWidget(
-            visible=False,
-        ),
-    ),
-))
-schema['description'].widget.visible = True
-schema['description'].schemata = 'default'
+# ~~~~~~~ To be implemented ~~~~~~~
+#     ComputedField('DepartmentTitle',
+#         expression="context.getDepartment() and context.getDepartment().Title() or ''",
+#         widget=ComputedWidget(
+#             visible=False,
+#         ),
+#     ),
+)
+#)
+# ~~~~~~~~~~ Irrelevant code for Odoo ~~~~~~~~~~~
+# schema['description'].widget.visible = True
+# schema['description'].schemata = 'default'
 
 
-class AnalysisCategory(BaseContent):
-    implements(IAnalysisCategory)
-    security = ClassSecurityInfo()
-    displayContentsTab = False
-    schema = schema
+class AnalysisCategory(models.Model, BaseOLiMSModel):
+    _name = "olims.analysis_category"
+    
+# ~~~~~~~~~~ Irrelevant code for Odoo ~~~~~~~~~~~    
+#     implements(IAnalysisCategory)
+#     security = ClassSecurityInfo()
+#     displayContentsTab = False
+#     schema = schema
 
     _at_rename_after_creation = True
 
     def _renameAfterCreation(self, check_auto_id=False):
         from lims.idserver import renameAfterCreation
         renameAfterCreation(self)
-
-    def getDepartments(self):
-        bsc = getToolByName(self, 'bika_setup_catalog')
-        deps = []
-        for d in bsc(portal_type='Department',
-                     inactive_state='active'):
-            deps.append((d.UID, d.Title))
-        return DisplayList(deps)
+# ~~~~~~~ To be implemented ~~~~~~~
+#     def getDepartments(self):
+#         bsc = getToolByName(self, 'bika_setup_catalog')
+#         deps = []
+#         for d in bsc(portal_type='Department',
+#                      inactive_state='active'):
+#             deps.append((d.UID, d.Title))
+#         return DisplayList(deps)
 
     def workflow_script_deactivat(self):
         # A instance cannot be deactivated if it contains services
@@ -79,5 +97,6 @@ class AnalysisCategory(BaseContent):
             transaction.get().abort()
             raise WorkflowException
 
-
-registerType(AnalysisCategory, PROJECTNAME)
+AnalysisCategory.initialze(schema)
+# ~~~~~~~~~~ Irrelevant code for Odoo ~~~~~~~~~~~
+# registerType(AnalysisCategory, PROJECTNAME)
