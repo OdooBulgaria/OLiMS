43dd154e743e517ad819f80dc3bd93304c240e12
yasir1brahim <yasir.ibrahim@gmail.com>
2015-09-16 15:48:38 +0500

    Re-factored 'invoice.py' and added it to Odoo model. Some fields and methods in invoice.py are not yet developed, so these are tagged as “To be Implemented”.

--- models/invoice.py
+++ models/invoice.py
@@ -1,32 +1,56 @@
-from dependencies.dependency import ClassSecurityInfo
+
+# ~~~~~~~~~~ Irrelevant code for Odoo ~~~~~~~~~~~
+
+#from dependencies.dependency import ClassSecurityInfo
+#from lims.content.bikaschema import BikaSchema
+#from lims.interfaces import IInvoice
+#from dependencies.dependency import DateTimeField, DateTimeWidget
+#from dependencies.dependency import DateTime
+#from dependencies.dependency import implements
+#from lims.config import ManageInvoices, ManageBika, PROJECTNAME\
+#from dependencies.dependency import PersistentMapping
+#from dependencies.dependency import View
+
+from dependencies.dependency import Decimal
+from dependencies.dependency import getToolByName
+from dependencies.dependency import safe_unicode
 from lims import bikaMessageFactory as _
 from lims.utils import t
-from lims.config import ManageInvoices, ManageBika, PROJECTNAME
-from lims.content.bikaschema import BikaSchema
-from lims.interfaces import IInvoice
-from dependencies.dependency import DateTime
-from dependencies.dependency import Decimal
-from dependencies.dependency import PersistentMapping
-from dependencies.dependency import *
-from dependencies.dependency import DateTimeField, DateTimeWidget
-from dependencies.dependency import View
-from dependencies.dependency import safe_unicode
-from dependencies.dependency import implements
-import sys
+from lims.utils import to_utf8
 
-schema = BikaSchema.copy() + Schema((
-    ReferenceField('Client',
-        required=1,
-        vocabulary_display_path_bound=sys.maxsize,
-        allowed_types=('Client',),
-        relationship='ClientInvoice',
-    ),
-    ReferenceField('AnalysisRequest',
-        required=1,
-        vocabulary_display_path_bound=sys.maxsize,
-        allowed_types=('AnalysisRequest',),
-        relationship='AnalysisRequestInvoice',
-    ),
+
+from datetime import date
+from openerp import fields, models
+from models.base_olims_model import BaseOLiMSModel
+from fields.string_field import StringField
+from fields.text_field import TextField
+from fields.boolean_field import BooleanField
+from fields.date_time_field import DateTimeField
+from fields.file_field import FileField
+from fields.reference_field import ReferenceField
+from fields.widget.widget import StringWidget, TextAreaWidget, BooleanWidget, FileWidget, DateTimeWidget
+
+import logging
+import sys
+_logger = logging.getLogger(__name__)
+
+
+#schema = BikaSchema.copy() + Schema((
+schema = (
+    # ~~~~~~~ To be implemented ~~~~~~~
+    # ReferenceField('Client',
+    #     required=1,
+    #     vocabulary_display_path_bound=sys.maxsize,
+    #     allowed_types=('Client',),
+    #     relationship='ClientInvoice',
+    # ),
+    # ReferenceField('AnalysisRequest',
+    #     required=1,
+    #     vocabulary_display_path_bound=sys.maxsize,
+    #     allowed_types=('AnalysisRequest',),
+    #     relationship='AnalysisRequestInvoice',
+    # ),
+
     DateTimeField('InvoiceDate',
         required=1,
         default_method='current_date',
@@ -36,65 +60,69 @@ schema = BikaSchema.copy() + Schema((
     ),
     TextField('Remarks',
         searchable=True,
-        default_content_type='text/plain',
-        allowed_content_types=('text/plain', ),
-        default_output_type="text/plain",
+        #default_content_type='text/plain',
+        #allowed_content_types=('text/plain', ),
+        #default_output_type="text/plain",
         widget=TextAreaWidget(
-            macro="bika_widgets/remarks",
+         #   macro="bika_widgets/remarks",
             label=_("Remarks"),
             append_only=True,
         ),
     ),
-    ComputedField('Subtotal',
-        expression='context.getSubtotal()',
-        widget=ComputedWidget(
-            label=_("Subtotal"),
-            visible=False,
-        ),
-    ),
-    ComputedField('VATAmount',
-        expression='context.getVATAmount()',
-        widget=ComputedWidget(
-            label=_("VAT Total"),
-            visible=False,
-        ),
-    ),
-    ComputedField('Total',
-        expression='context.getTotal()',
-        widget=ComputedWidget(
-            label=_("Total"),
-            visible=False,
-        ),
-    ),
-    ComputedField('ClientUID',
-        expression='here.getClient() and here.getClient().UID()',
-        widget=ComputedWidget(
-            visible=False,
-        ),
-    ),
-    ComputedField('InvoiceSearchableText',
-        expression='here.getInvoiceSearchableText()',
-        widget=ComputedWidget(
-            visible=False,
-        ),
-    ),
-),
+    # ~~~~~~~ To be implemented ~~~~~~~
+    # ComputedField('Subtotal',
+    #     expression='context.getSubtotal()',
+    #     widget=ComputedWidget(
+    #         label=_("Subtotal"),
+    #         visible=False,
+    #     ),
+    # ),
+    # ComputedField('VATAmount',
+    #     expression='context.getVATAmount()',
+    #     widget=ComputedWidget(
+    #         label=_("VAT Total"),
+    #         visible=False,
+    #     ),
+    # ),
+    # ComputedField('Total',
+    #     expression='context.getTotal()',
+    #     widget=ComputedWidget(
+    #         label=_("Total"),
+    #         visible=False,
+    #     ),
+    # ),
+    # ComputedField('ClientUID',
+    #     expression='here.getClient() and here.getClient().UID()',
+    #     widget=ComputedWidget(
+    #         visible=False,
+    #     ),
+    # ),
+    # ComputedField('InvoiceSearchableText',
+    #     expression='here.getInvoiceSearchableText()',
+    #     widget=ComputedWidget(
+    #         visible=False,
+    #     ),
+    # ),
 )
 
-TitleField = schema['title']
-TitleField.required = 0
-TitleField.widget.visible = False
+# ~~~~~~~~~~ Irrelevant code for Odoo ~~~~~~~~~~~
+# TitleField = schema['title']
+# TitleField.required = 0
+# TitleField.widget.visible = False
 
 
-class InvoiceLineItem(PersistentMapping):
-    pass
+# ~~~~~~~~~~ Irrelevant code for Odoo ~~~~~~~~~~~
+# class InvoiceLineItem(PersistentMapping):
+#     pass
 
 
-class Invoice(BaseFolder):
-    implements(IInvoice)
-    security = ClassSecurityInfo()
-    displayContentsTab = False
-    schema = schema
+class Invoice(models.Model, BaseOLiMSModel): #(BaseFolder):
+    _name='olims.invoice'
+# ~~~~~~~~~~ Irrelevant code for Odoo ~~~~~~~~~~~
+    # implements(IInvoice)
+    # security = ClassSecurityInfo()
+    # displayContentsTab = False
+    # schema = schema
 
     _at_rename_after_creation = True
 
@@ -106,25 +134,29 @@ class Invoice(BaseFolder):
         """ Return the Invoice Id as title """
         return safe_unicode(self.getId()).encode('utf-8')
 
-    security.declareProtected(View, 'getSubtotal')
+# ~~~~~~~~~~ Irrelevant code for Odoo ~~~~~~~~~~~
+    #security.declareProtected(View, 'getSubtotal')
 
     def getSubtotal(self):
         """ Compute Subtotal """
         return sum([float(obj['Subtotal']) for obj in self.invoice_lineitems])
 
-    security.declareProtected(View, 'getVATAmount')
+# ~~~~~~~~~~ Irrelevant code for Odoo ~~~~~~~~~~~
+    #security.declareProtected(View, 'getVATAmount')
 
     def getVATAmount(self):
         """ Compute VAT """
         return Decimal(self.getTotal()) - Decimal(self.getSubtotal())
 
-    security.declareProtected(View, 'getTotal')
+# ~~~~~~~~~~ Irrelevant code for Odoo ~~~~~~~~~~~
+    #security.declareProtected(View, 'getTotal')
 
     def getTotal(self):
         """ Compute Total """
         return sum([float(obj['Total']) for obj in self.invoice_lineitems])
 
-    security.declareProtected(View, 'getInvoiceSearchableText')
+# ~~~~~~~~~~ Irrelevant code for Odoo ~~~~~~~~~~~
+    #security.declareProtected(View, 'getInvoiceSearchableText')
 
     def getInvoiceSearchableText(self):
         """ Aggregate text of all line items for querying """
@@ -138,10 +170,13 @@ class Invoice(BaseFolder):
         """ dispatch order """
         self.setDateDispatched(DateTime())
 
-    security.declarePublic('current_date')
+# ~~~~~~~~~~ Irrelevant code for Odoo ~~~~~~~~~~~
+    #security.declarePublic('current_date')
 
     def current_date(self):
         """ return current date """
         return DateTime()
 
-registerType(Invoice, PROJECTNAME)
+# ~~~~~~~~~~ Irrelevant code for Odoo ~~~~~~~~~~~
+#registerType(Invoice, PROJECTNAME)
+Invoice.initialze(schema)
