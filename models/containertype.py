d4143f27bb21780994f8aae0416cf54fd6539919
yasir1brahim <yasir.ibrahim@gmail.com>
2015-09-17 16:30:28 +0500

    Copied the 'containertype.py' file  in Odoo model from the content folder of Bika  to use/convert Bika content class as Odoo model

---
+++ models/containertype.py
@@ -0,0 +1,44 @@
+from dependencies.dependency import ClassSecurityInfo
+from dependencies.dependency import *
+from dependencies.dependency import getToolByName
+from lims import bikaMessageFactory as _
+from lims.utils import t
+from lims.content.bikaschema import BikaSchema
+from lims.config import PROJECTNAME
+
+schema = BikaSchema.copy() + Schema((
+))
+schema['description'].widget.visible = True
+schema['description'].schemata = 'default'
+
+class ContainerType(BaseContent):
+    security = ClassSecurityInfo()
+    displayContentsTab = False
+    schema = schema
+
+    _at_rename_after_creation = True
+    def _renameAfterCreation(self, check_auto_id=False):
+        from lims.idserver import renameAfterCreation
+        renameAfterCreation(self)
+
+    def getContainers(self):
+        """Return a list of all containers of this type
+        """
+        _containers = []
+        for container in self.bika_setup.bika_containers.objectValues():
+            containertype = container.getContainerType()
+            if containertype and containertype.UID() == self.UID():
+                _containers.append(container)
+        return _containers
+
+registerType(ContainerType, PROJECTNAME)
+
+def ContainerTypes(self, instance=None, allow_blank=False):
+    instance = instance or self
+    bsc = getToolByName(instance, 'bika_setup_catalog')
+    items = []
+    for o in bsc(portal_type='ContainerType',
+                 sort_on = 'sortable_title'):
+        items.append((o.UID, o.Title))
+    items = allow_blank and [['','']] + list(items) or list(items)
+    return DisplayList(items)
