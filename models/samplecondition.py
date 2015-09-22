ae4829cdb65275753f6c3a4ca728bf3a4b266acc
yasir1brahim <yasir.ibrahim@gmail.com>
2015-09-22 15:07:23 +0500

    Re-factored the code of 'samplecondition.py' file to add it as a Odoo model. Some fields classes and method are not yet to be developed, so these are tagged as “To be Implemented”.

---
+++ models/samplecondition.py
@@ -0,0 +1,47 @@
+# ~~~~~~~~~~ Irrelevant code for Odoo ~~~~~~~~~~~
+# from dependencies.dependency import ClassSecurityInfo
+# from dependencies.dependency import BaseFolder
+# from dependencies.dependency import DisplayList
+# from dependencies.dependency import Schema
+# from dependencies.dependency import registerType
+# from dependencies.dependency import getToolByName
+# from lims.config import PROJECTNAME
+# from lims.content.bikaschema import BikaSchema
+#
+# schema = BikaSchema.copy() + Schema((
+#
+# ))
+#
+schema = ()
+# schema['description'].schemata = 'default'
+# schema['description'].widget.visible = True
+#
+from openerp import fields, models
+from models.base_olims_model import BaseOLiMSModel
+
+
+class SampleCondition(models.Model, BaseOLiMSModel): #BaseFolder
+    _name='olims.sample_condition'
+    # security = ClassSecurityInfo()
+    # displayContentsTab = False
+    # schema = schema
+
+    _at_rename_after_creation = True
+
+    def _renameAfterCreation(self, check_auto_id=False):
+        from lims.idserver import renameAfterCreation
+        renameAfterCreation(self)
+
+#registerType(SampleCondition, PROJECTNAME)
+SampleCondition.initialze(schema)
+
+def SampleConditions(self, instance=None, allow_blank=False):
+    instance = instance or self
+    bsc = getToolByName(instance, 'bika_setup_catalog')
+    items = []
+    for sm in bsc(portal_type='SampleCondition',
+                  inactive_state='active',
+                  sort_on='sortable_title'):
+        items.append((sm.UID, sm.Title))
+    items = allow_blank and [['', '']] + list(items) or list(items)
+    return DisplayList(items)
