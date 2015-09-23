0c0f4c142bc2d61cc2dd5b86274e00b9f8d9e3eb
yasir1brahim <yasir.ibrahim@gmail.com>
2015-09-23 19:11:50 +0500

    Re-factored the code of 'attachmenttype.py' file to add it as a Odoo model. Some fields classes and method are not yet to be developed, so these are tagged as “To be Implemented”.

--- models/attachmenttype.py
+++ models/attachmenttype.py
@@ -1,27 +1,37 @@
 """AttachmentType - the type of attachment
 """
-from dependencies.dependency import ClassSecurityInfo
-from dependencies.dependency import *
-from lims.content.bikaschema import BikaSchema
-from lims.config import PROJECTNAME
-import sys
-from lims import bikaMessageFactory as _
-from lims.utils import t
-from dependencies.dependency import implements
+# ~~~~~~~~~~ Irrelevant code for Odoo ~~~~~~~~~~~
+# from dependencies.dependency import ClassSecurityInfo
+# from dependencies.dependency import *
+# from lims.content.bikaschema import BikaSchema
+# from lims.config import PROJECTNAME
+# import sys
+# from lims import bikaMessageFactory as _
+# from lims.utils import t
+# from dependencies.dependency import implements
+#
+# schema = BikaSchema.copy()
 
-schema = BikaSchema.copy()
+from openerp import fields, models
+from models.base_olims_model import BaseOLiMSModel
 
-schema['description'].widget.visible = True
-schema['description'].schemata = 'default'
 
-class AttachmentType(BaseContent):
-    security = ClassSecurityInfo()
-    displayContentsTab = False
-    schema = schema
+schema = ()
+
+# schema['description'].widget.visible = True
+# schema['description'].schemata = 'default'
+
+class AttachmentType(models.Model, BaseOLiMSModel): #BaseContent
+    _name = 'olims.attachment_type'
+    # security = ClassSecurityInfo()
+    # displayContentsTab = False
+    # schema = schema
 
     _at_rename_after_creation = True
     def _renameAfterCreation(self, check_auto_id=False):
         from lims.idserver import renameAfterCreation
         renameAfterCreation(self)
 
-registerType(AttachmentType, PROJECTNAME)
+#registerType(AttachmentType, PROJECTNAME)
+
+AttachmentType.initialze(schema)
